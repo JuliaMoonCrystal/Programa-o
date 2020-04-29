6d90e67b8e261d338e953cd913bf6bd0e59b859f
JuliaMoonCrystal <47614833+JuliaMoonCrystal@users.noreply.github.com>
2020-04-29 14:04:53 -0300

    Add files via upload

---
+++ Recriando_Jogos/src/Exercicios_Aleatorios/TesteImagem.java
@@ -0,0 +1,28 @@
+package Exercicios_Aleatorios;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TesteImagem extends JFrame {
+
+	ImageIcon imagem =new ImageIcon(getClass().getResource("leon.jpg"));
+	JLabel label=new JLabel(imagem);
+	
+	
+	public TesteImagem() {
+		
+		add(label);
+		setSize(400,500);
+		setTitle("Testando como colocar uma imagem");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(this);
+		setVisible(true);
+	}
+	
+	public static void main(String[]args) {
+		new TesteImagem();
+	}
+	
+	
+}
