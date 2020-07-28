01b67cd8ba4a5549ec4350bcf140c07dff40060a
Julia Farias da Rocha <jufarias20002@gmail.com>
2020-07-28 19:35:19 -0300

    .

--- Recriando_Jogos/src/Desafios_Digital_House/fatorial.java
+++ Recriando_Jogos/src/Desafios_Digital_House/fatorial.java
@@ -4,9 +4,7 @@ public class fatorial {
 	public static void main(String[] args) {
 		int fatorial=5;
 		System.out.println("O fatorial de "+fatorial+" é : "+CauculaFatorial(fatorial));
-		
 	}
-
 	private static int CauculaFatorial(int fatorial) {
 		int resultado=1;
 		
