2ea76fe0a9c89883c20027b7c2c636f8d4dd2835
Julia Farias da Rocha <jufarias20002@gmail.com>
2020-07-28 19:49:24 -0300

    .

---
+++ Recriando_Jogos/src/Controle_de_gastos/Gastos.java
@@ -0,0 +1,34 @@
+package Controle_de_gastos;
+
+public class Gastos {
+ private double conta_luz;
+ private double conta_agua;
+ private double conta_gas;
+ private double outros_gastos;
+ 
+public double getConta_luz() {
+	return conta_luz;
+}
+public void setConta_luz(double conta_luz) {
+	this.conta_luz = conta_luz;
+}
+public double getConta_agua() {
+	return conta_agua;
+}
+public void setConta_agua(double conta_agua) {
+	this.conta_agua = conta_agua;
+}
+public double getConta_gas() {
+	return conta_gas;
+}
+public void setConta_gas(double conta_gas) {
+	this.conta_gas = conta_gas;
+}
+public double getOutros_gastos() {
+	return outros_gastos;
+}
+public void setOutros_gastos(double outros_gastos) {
+	this.outros_gastos = outros_gastos;
+}
+ 
+}
