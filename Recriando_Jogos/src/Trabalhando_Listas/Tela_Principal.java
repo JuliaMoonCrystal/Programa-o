d9cf47bcce004497b9282ebae8e5b64848465998
Julia Farias da Rocha <jufarias20002@gmail.com>
2020-09-23 23:42:17 -0300

    Update Tela_Principal.java

--- Recriando_Jogos/src/Trabalhando_Listas/Tela_Principal.java
+++ Recriando_Jogos/src/Trabalhando_Listas/Tela_Principal.java
@@ -9,20 +9,23 @@ public class Tela_Principal {
 		JFrame janela= new JFrame(); //Criamos a instancia do jframe;
 		
 		JButton adicionar= new JButton("Adicionar novo contato");
-		//JButton excluir= new JButton("Excluir contato");
-	//	JButton Pesquisar= new JButton("Pesquisar contato");
-	//	JButton atualizar= new JButton("Atualizar contato");
+		JButton excluir= new JButton("Excluir contato");
+		JButton Pesquisar= new JButton("Pesquisar contato");
+		JButton atualizar= new JButton("Atualizar contato");
 		
 		
-		adicionar.setBounds( 130 , 100 , 190 ,  40 );
-		//excluir.setBounds(190 , 100 , 190 ,  40 );
-		//Pesquisar.setBounds(130 , 100 , 190 ,  40 );
-		//atualizar.setBounds(130 , 100 , 190 ,  40 );
+		adicionar.setBounds( 150 , 100 , 190 ,  40 );
+		excluir.setBounds(150 , 150 , 190 ,  40 );
+		Pesquisar.setBounds(150 , 200 , 190 ,  40 );
+		atualizar.setBounds(150 , 250 , 190 ,  40 );
 		
-		
+		//Aqui adicionamos os botões na tela 
 		janela.add(adicionar);
-		//janela.add(excluir);
-        janela.setTitle("Janela Principal");			
+		janela.add(excluir);
+		janela.add(Pesquisar);
+		janela.add(atualizar);
+		
+        janela.setTitle("Agenda de Contatos");			
 		janela.setSize(500,500);
 		janela.setLayout(null);
 		janela.setVisible(true);
