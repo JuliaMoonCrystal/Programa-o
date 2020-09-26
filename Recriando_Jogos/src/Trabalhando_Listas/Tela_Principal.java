f70e416efa9e45a4bbca770b27b6e2817dc4934d
Julia Farias da Rocha <jufarias20002@gmail.com>
2020-09-25 22:46:49 -0300

    Update Tela_Principal.java
    Sufoco de fazer esses botões terem uma ação.
    Pelo menos foi

--- Recriando_Jogos/src/Trabalhando_Listas/Tela_Principal.java
+++ Recriando_Jogos/src/Trabalhando_Listas/Tela_Principal.java
@@ -1,5 +1,7 @@
 package Trabalhando_Listas;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,7 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
-public class Tela_Principal  {
+public class Tela_Principal implements ActionListener {
 	public static void main(String [] args) {
 		JFrame janela= new JFrame(); //Criamos a instancia do jframe;
 		
@@ -31,6 +33,40 @@ public class Tela_Principal  {
 		janela.add(Pesquisar);
 		janela.add(atualizar);
 		
+		
+	  //adicionamos ações para os botões
+		adicionar.addActionListener(new ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			   if(e.getSource()==adicionar) {
+				   System.out.println("Botão adicionar acionado");
+			   }
+			}
+		});
+		
+		excluir.addActionListener(new ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			   if(e.getSource()==excluir) {
+				   System.out.println("Botão excluir acionado");
+			   }
+			}
+		});
+		
+		Pesquisar.addActionListener(new ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			   if(e.getSource()==Pesquisar) {
+				   System.out.println("Botão Pesquisar acionado");
+			   }
+			}
+		});
+		
+		atualizar.addActionListener(new ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+			   if(e.getSource()==atualizar) {
+				   System.out.println("Botão Atualizar acionado");
+			   }
+			}
+		});
+		
         janela.setTitle("Agenda de Contatos");			
 		janela.setSize(500,500);
 		janela.setLayout(null);
@@ -38,5 +74,10 @@ public class Tela_Principal  {
 	
 	}
 
+	@Override
+	public void actionPerformed(java.awt.event.ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
